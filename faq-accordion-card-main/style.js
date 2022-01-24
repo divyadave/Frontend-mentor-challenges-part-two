c7b8f148c347c9b43c12d84298878f1c62895d7d
Divya Dave <divya@automatapi.com>
2022-01-24 11:50:04 +0530

    FAQ design

---
+++ faq-accordion-card-main/style.js
@@ -0,0 +1,10 @@
+var questions = document.querySelectorAll(".question");
+questions.forEach((question) => question.addEventListener('click', function() {
+    if(question.parentNode.classList.contains("active")){
+        question.parentNode.classList.toggle("active")
+    }
+else{
+questions.forEach(question => question.parentNode.classList.remove("active"))
+question.parentNode.classList.add("active")
+    }
+}))
